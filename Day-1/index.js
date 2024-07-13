2bd6e3b8763b0cc00819ded622c949a9b4c9254f
Sujan Timalsina <167009579+codewithsuzan@users.noreply.github.com>
2024-07-13 13:32:20 +0545

    Add files via upload

---
+++ Day-1/index.js
@@ -0,0 +1,91 @@
+// Activity 1
+// Task 1:
+// var a=10;
+// console.log(a);
+
+// Task 2:
+// let str="55"
+// console.log(str)
+
+// Activity 2
+// Task 3:
+
+// const val=true
+// console.log(val)
+
+// Activity 3
+// Task 4:
+
+// Number
+// let num = 42;
+// console.log(typeof num); // Output: "number"
+
+// // String
+// let str = "Hello, world!";
+// console.log(typeof str); // Output: "string"
+
+// // Boolean
+// let bool = true;
+// console.log(typeof bool); // Output: "boolean"
+
+// // Object
+// let obj = { name: "John", age: 30 };
+// console.log(typeof obj); // Output: "object"
+
+// // Array
+// let arr = [1, 2, 3, 4, 5];
+// console.log(typeof arr); // Output: "object" 
+
+// Activity 4
+//  Task 5: 
+// let a=10;
+// console.log(a)
+
+// a=20
+// console.log(a)
+
+// Activity 5
+// Task 6:
+
+// const a=10
+// console.log(a)
+// a=20
+// console.log(a)
+// TypeError: Assignment to constant variable.
+// This error occurs because once a variable is declared with const and assigned a value, that value cannot be changed throughout the rest of the program. 
+
+
+// Feature Request
+// Task 1:
+
+// //String
+// let str = "Hello, world!";
+// console.log("Value:", str, "Type:", typeof str);
+
+// //Number
+// let num = 42;
+// console.log("Value:", num, "Type:", typeof num);
+
+// //BigInt
+// let bigIntNum = 1234567890123456789012345678901234567890;
+// console.log("Value:", bigIntNum, "Type:", typeof bigIntNum);
+
+// //Boolean
+// let bool = true;
+// console.log("Value:", bool, "Type:", typeof bool);
+
+// //Undefined
+// let undefinedVar;
+// console.log("Value:", undefinedVar, "Type:", typeof undefinedVar);
+
+// //Null
+// let nullVar = null;
+// console.log("Value:", nullVar, "Type:", typeof nullVar);
+
+// //Symbol
+// let symbolVar = Symbol("foo");
+// console.log("Value:", symbolVar, "Type:", typeof symbolVar);
+
+// //Object
+// let obj = { name: "John", age: 30 };
+// console.log("Value:", obj, "Type:", typeof obj);
